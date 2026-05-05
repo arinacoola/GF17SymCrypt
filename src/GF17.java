b448e0f35f20514b915c8d4905fc49fb27f3fa2c
arinacoola <a.zvarunchyk@gmail.com>
2026-05-05 15:24:19 +0300

    mul

--- src/GF17.java
+++ src/GF17.java
@@ -6,5 +6,21 @@ public class GF17 {
     public static int add(int u, int v ) {
         return u ^ v;
     }
+
+    public static int mul(int u,int v) {
+        int res= 0;
+        while (v!= 0) {
+            if ((v & 1) != 0) {
+                res^= u;
+            }
+            v>>= 1;
+            u<<= 1;
+            if ((u &fieldSize)!= 0) {
+                u^= poly;
+            }
+        }
+        return res& (fieldSize- 1);
+    }
+
 }
 
