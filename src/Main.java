5f07dc3992e8659cba9645240b4cfa24f1597b34
arinacoola <a.zvarunchyk@gmail.com>
2026-05-05 16:15:39 +0300

    f_i-збалансовані,g_i-теж

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         int[] f=VectorBooleanFunc.buildF();
-        int[][] fBits = CoordinateFunc.decompose(f);
-        int x = 12345;
-        int restored= 0;
-        for (int i = 0;i < 17;i++){
-            restored |= fBits[i][x]<< i;
+        int[] g = VectorBooleanFunc.buildG();
+        int[][] fBits =CoordinateFunc.decompose(f);
+        int[][] gBits = CoordinateFunc.decompose(g);
+        int[] fDis=BooleanProp.allDisbalance(fBits);
+        int[] gDis =BooleanProp.allDisbalance(gBits);
+        System.out.println("дисбаланс f:");
+        for (int i = 0; i < 17; i++) {
+            System.out.println("f" + (i + 1) +": " + fDis[i]);
         }
-        System.out.println("f[x] = " + f[x]);
-        System.out.println("restored = "+restored);
-        if (f[x] == restored){
-            System.out.println("одаддададададад");
-        }
-        else {
-            System.out.println("нононо");
+        System.out.println("дисбаланс g:");
+        for (int i = 0; i < 17; i++) {
+            System.out.println("g" + (i+1) + ": " + gDis[i]);
         }
     }
-
-
 }
+
+
