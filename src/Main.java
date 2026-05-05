9f971512dfb07de587919c5408fa128a6c6a9c11
arinacoola <a.zvarunchyk@gmail.com>
2026-05-05 15:53:02 +0300

    перевірка пройдена

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         int[] f=VectorBooleanFunc.buildF();
-        int[] g = VectorBooleanFunc.buildG();
-        System.out.println(f[0]);
-        System.out.println(f[1]);
-        System.out.println(f[12345]);
-        System.out.println(g[0]);
-        System.out.println(g[1]);
-        System.out.println(g[12345]);
+        int[][] fBits = CoordinateFunc.decompose(f);
+        int x = 12345;
+        int restored= 0;
+        for (int i = 0;i < 17;i++){
+            restored |= fBits[i][x]<< i;
+        }
+        System.out.println("f[x] = " + f[x]);
+        System.out.println("restored = "+restored);
+        if (f[x] == restored){
+            System.out.println("одаддададададад");
+        }
+        else {
+            System.out.println("нононо");
+        }
     }
+
+
 }
