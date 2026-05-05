12db5bd2f73b02dfb5c9bda2ea80d16d478a5ab9
arinacoola <a.zvarunchyk@gmail.com>
2026-05-05 15:21:58 +0300

    add

--- src/GF17.java
+++ src/GF17.java
@@ -2,5 +2,9 @@ public class GF17 {
     private static final int N = 17;
     private static final int fieldSize = 1 << N;
     private static final int poly = (1 << 17) | (1 << 3) | 1;
+
+    public static int add(int u, int v ) {
+        return u ^ v;
+    }
 }
 
